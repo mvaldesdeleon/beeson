a044091b8c2d284c6d47d2f368c7f9ea6d2ed3c3
Martín Valdés de León <m.valdesdeleon@sovanta.com>
2017-05-21 02:15:36 +0200

    Leverages binary encoding to `msgpack`, and processes `Date` objects manually.

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+const msgpack = require('msgpack');
+const cloneDeepWith = require('lodash.clonedeepwith');
+
 function serialize(data) {
-    return JSON.stringify(data);
+    data = cloneDeepWith(data, (value, key) => value instanceof Date ? {_type: 'Date', _data: value.toISOString()} : void 0);
+
+    return msgpack.pack(data);
 }
 
 function deserialize(data) {
-    return JSON.parse(data);
+    data = msgpack.unpack(data);
+
+    return cloneDeepWith(data, (value, key) => value && typeof value === 'object' && '_type' in value && '_data' in value && value._type === 'Date' && value._data ? new Date(value._data) : void 0);
 }
 
 module.exports = {
