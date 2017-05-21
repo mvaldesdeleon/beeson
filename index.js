fe68ecc925badd78bedcda09a280f8d624ecec09
Martín Valdés de León <m.valdesdeleon@sovanta.com>
2017-05-21 02:33:16 +0200

    TODOs

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const msgpack = require('msgpack');
 const cloneDeepWith = require('lodash.clonedeepwith');
 
+// TODO: add support for `undefined`
+// TODO: test NaN, +/- Infinity, -0
+// TODO: think more things to test
+
 function serialize(data) {
     data = cloneDeepWith(data, (value, key) => value instanceof Date ? {_type: 'Date', _data: value.toISOString()} : void 0);
 
